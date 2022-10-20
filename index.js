b464520899444d2a24d0c2fa8f54544c364fe442
Santiago Garcia Solarte <santgaso@gmail.com>
2022-10-19 20:59:02 -0500

    Se añade Node js, y una funciona para asignar un id a cada nuevo jugador

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("Hola Node")
+
+const express = require ("express")
+
+const app = express()
+
+const jugadores = []
+class Jugador {
+    constructor(id){
+        this.id = id
+    }
+}
+
+app.get("/unirse",(req, res)=> {
+    const id =  `${Math.round((Math.random())*10000)}`
+    const jugador = new Jugador(id)
+    jugadores.push(jugador)
+    res.send(id)
+    console.log(jugadores)
+})
+
+app.listen(8080,()=> {
+    console.log("El servidor arranco")
+})
+ 
+
